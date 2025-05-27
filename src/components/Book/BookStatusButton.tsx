6f6056331e4fc84681d401364ddd2c70846e8fbf
Dmitriy Khutornoy <thth13q@gmail.com>
2025-05-27 22:57:47 +0300

    fix bookstatusbutton

--- src/components/Book/BookStatusButton.tsx
+++ src/components/Book/BookStatusButton.tsx
@@ -9,7 +9,7 @@ import {
   faListCheck,
   faTrash,
 } from '@fortawesome/free-solid-svg-icons';
-import { Fragment, useContext, useEffect } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import { Menu, MenuButton, MenuItem, Transition } from '@headlessui/react';
 import { Book, ReadCategory } from '@/src/types';
 import { getUserBookCategory } from '@/src/lib/utils';
@@ -21,12 +21,14 @@ const BookStatusButton = ({ book }: { book: Book }) => {
   const authContext = useContext(AuthContext);
   const userId = authContext?.userId;
   const { profile, addBookToProfile } = useUserProfile();
+  const [showMenu, setShowMenu] = useState(false);
 
   const { currentCategory, setCurrentCategory } = useBook();
 
   const handleCategoryChange = async (newCategory: ReadCategory) => {
     if (userId) {
       addBookToProfile(book._id, userId, newCategory, currentCategory ? currentCategory : undefined);
+      setShowMenu(false);
     }
   };
 
@@ -68,7 +70,10 @@ const BookStatusButton = ({ book }: { book: Book }) => {
     <div className="mt-6 space-y-3">
       <Menu as="div" className="relative">
         {currentCategory ? (
-          <MenuButton className="w-full px-4 py-2.5 bg-blue-100 text-blue-700 rounded-lg flex items-center justify-between whitespace-nowrap">
+          <MenuButton
+            onClick={() => setShowMenu(!showMenu)}
+            className="w-full px-4 py-2.5 bg-blue-100 text-blue-700 rounded-lg flex items-center justify-between whitespace-nowrap"
+          >
             <span className="flex items-center">
               <FontAwesomeIcon
                 icon={menuItems.find((item) => item.category === currentCategory)?.icon || faBookOpen}
@@ -79,7 +84,10 @@ const BookStatusButton = ({ book }: { book: Book }) => {
             <FontAwesomeIcon icon={faCheck} />
           </MenuButton>
         ) : (
-          <MenuButton className="w-full px-4 py-2.5 bg-blue-600 text-white rounded-lg hover:bg-blue-700 flex items-center justify-between">
+          <MenuButton
+            onClick={() => setShowMenu(!showMenu)}
+            className="w-full px-4 py-2.5 bg-blue-600 text-white rounded-lg hover:bg-blue-700 flex items-center justify-between"
+          >
             <span className="flex items-center">
               <FontAwesomeIcon icon={faBookmark} className="mr-2" />
               Reading Status
@@ -90,6 +98,7 @@ const BookStatusButton = ({ book }: { book: Book }) => {
 
         <Transition
           as={Fragment}
+          show={showMenu}
           enter="transition ease-out duration-100"
           enterFrom="transform opacity-0 scale-95"
           enterTo="transform opacity-100 scale-100"
